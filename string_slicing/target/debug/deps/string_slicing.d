227d15d1b475f9c64b60949c5da351918ea19f60
Wells <zwells@harsco.com>
2020-05-14 16:34:42 +0100

    string slicing examps

---
+++ string_slicing/target/debug/deps/string_slicing.d
@@ -0,0 +1,5 @@
+C:\cygwin64\home\zwells\rustProjects\string_slicing\target\debug\deps\string_slicing.exe: src\main.rs
+
+C:\cygwin64\home\zwells\rustProjects\string_slicing\target\debug\deps\string_slicing.d: src\main.rs
+
+src\main.rs:
---
+++ string_slicing/target/debug/string_slicing.d
@@ -0,0 +1,1 @@
+C:\cygwin64\home\zwells\rustProjects\string_slicing\target\debug\string_slicing.exe: C:\cygwin64\home\zwells\rustProjects\string_slicing\src\main.rs
