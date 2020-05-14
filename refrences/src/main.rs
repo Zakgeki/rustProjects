2ba52b78a61801c801a234bf1db9a0a329aa3fc4
Wells <zwells@harsco.com>
2020-05-14 16:01:36 +0100

    Added mutable reference example

--- refrences/src/main.rs
+++ refrences/src/main.rs
@@ -9,8 +9,23 @@ fn main() {
 									   // `calc_length`
 
 	println!( "The length of '{}' is {}", s1, len );
+
+
+	// the code bellow passes the reference to `add_bar` with the mut
+	// flag/operater so that we can change s2, but retain owenership
+	let mut s2 = String::from( "foo" );
+
+	print!( "Change '{}'", s2 );
+
+	add_bar( &mut s2 );
+
+	println!( " to '{}'", s2 );
 }
 
 fn calc_length( s: &String ) -> usize { // takes a string by reference
 	s.len( )
 } // returns the length of the string
+
+fn add_bar( some_string: &mut String ) {
+	some_string.push_str( "bar" );
+}
