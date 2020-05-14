93fa066cd3f563ddefa15e3b79303dc95221b40d
Wells <zwells@harsco.com>
2020-05-14 16:57:06 +0100

    Basic struct examples

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+C:\cygwin64\home\zwells\rustProjects\structs\target\debug\deps\structs.exe: src\main.rs
+
+C:\cygwin64\home\zwells\rustProjects\structs\target\debug\deps\structs.d: src\main.rs
+
+src\main.rs:
