3075f344cb94d1b2ecc4d87bb649a52a5ecfbbc3
Zakgeki <45861741+Zakgeki@users.noreply.github.com>
2020-05-18 10:11:10 -0400

    did some basic gui stuff

---
+++ gui_test/target/debug/build/winapi-3ceddf44cbad5951/build_script_build-3ceddf44cbad5951.d
@@ -0,0 +1,5 @@
+C:\cygwin64\home\zwells\rustProjects\gui_test\target\debug\build\winapi-3ceddf44cbad5951\build_script_build-3ceddf44cbad5951.exe: C:\Users\zwells\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
+
+C:\cygwin64\home\zwells\rustProjects\gui_test\target\debug\build\winapi-3ceddf44cbad5951\build_script_build-3ceddf44cbad5951.d: C:\Users\zwells\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs
+
+C:\Users\zwells\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.8\build.rs:
---
+++ gui_test/target/debug/deps/gui_test.d
@@ -0,0 +1,5 @@
+C:\cygwin64\home\zwells\rustProjects\gui_test\target\debug\deps\gui_test.exe: src\main.rs
+
+C:\cygwin64\home\zwells\rustProjects\gui_test\target\debug\deps\gui_test.d: src\main.rs
+
+src\main.rs:
